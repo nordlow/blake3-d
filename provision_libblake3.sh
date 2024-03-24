0ff76045bd1264cbaa220354325ad4db086d3122
Per Nordlöw <per.nordlow@gmail.com>
2024-03-24 16:09:27 +0100

    Update provision_libblake3.sh: Use silent call to make for now to not obscure output when run from dub

--- provision_libblake3.sh
+++ provision_libblake3.sh
@@ -8,5 +8,5 @@ TARGET_ROOT="BLAKE3-build"
 mkdir -p "${TARGET_ROOT}"
 pushd "${TARGET_ROOT}"
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS="-march=native" ../"${SOURCE_ROOT}"
-make
+make --quiet
 popd
