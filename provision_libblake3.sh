569d9cc972ebfd43aeb2d1de60584acb4d9aa5d6
Per Nordlöw <per.nordlow@gmail.com>
2024-03-24 16:12:48 +0100

    Update provision_libblake3.sh: Reduce output a bit more

--- provision_libblake3.sh
+++ provision_libblake3.sh
@@ -6,7 +6,10 @@ SOURCE_ROOT="BLAKE3/c"
 TARGET_ROOT="BLAKE3-build"
 
 mkdir -p "${TARGET_ROOT}"
-pushd "${TARGET_ROOT}"
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS="-march=native" ../"${SOURCE_ROOT}"
+pushd "${TARGET_ROOT}" > /dev/null
+
+# ok to use "-march=native" because of no distribution
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS="-march=native" ../"${SOURCE_ROOT}" > /dev/null
 make --quiet
-popd
+
+popd > /dev/null
