7c6aba16b7313fa0c878f64ac89c1377167b0f4d
Per Nordlöw <per.nordlow@gmail.com>
2024-05-08 07:51:01 +0200

    Update provision.sh:

--- provision.sh
+++ provision.sh
@@ -8,10 +8,22 @@ TARGET_ROOT="BLAKE3-build"
 mkdir -p "${TARGET_ROOT}"
 pushd "${TARGET_ROOT}" > /dev/null
 
-if ! command -v cmake >/dev/null 2>&1; then
-    echo "Installing cmake ..."
-    sudo apt install cmake
-fi
+install_apt_packages_of_executables() {
+	local packages=()
+	for command in "$@"; do
+		if ! command -v "$command" >/dev/null 2>&1; then
+			packages+=("$command")
+		fi
+	done
+	if [[ ${#packages[@]} -gt 0 ]]; then
+		package_list="${packages[@]}"
+		echo "Installing missing APT packages: $package_list ..."
+		sudo apt install "${packages[@]}"
+	fi
+}
+
+tools=("cmake")
+install_apt_packages_of_executables "${tools[@]}"
 
 # ok to use "-march=native" because of no distribution
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS="-march=native" ../"${SOURCE_ROOT}"
